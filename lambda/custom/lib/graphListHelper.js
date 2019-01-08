5debd71f53a7949918a40a9fed03202a405fdac7
Jens Bühl <jens.buehl@outlook.com>
2019-01-08 17:38:16 +0100

    Refactoring, use async await to improve readability

--- lambda/custom/lib/graphListHelper.js
+++ lambda/custom/lib/graphListHelper.js
@@ -1,62 +1,68 @@
+function getShoppingFilter(listName)
+{
+    return `contains(name,'einkauf')
+            or contains(name,'shop')
+            or contains(name,'grocery')
+            or contains(name,'${listName}')`;
+}
+
+function getTargetShoppingLists(graphClient, listName){
+    var filter = getShoppingFilter(listName);
+    return graphClient
+        .api('/me/outlook/taskFolders')
+        .filter(filter)
+        .count(true)
+        .get()
+}
+
+function addTaskToFolder(graphClient, targetListId, graphTaskItem){
+    return graphClient
+        .api(`/me/outlook/taskFolders/${targetListId}/tasks`)
+        .post(graphTaskItem)
+}
+
+function addTask(graphClient, graphTaskItem){
+    return graphClient
+        .api(`/me/outlook/tasks`)
+        .post(graphTaskItem)
+}
+
+function createList(graphClient, listName){
+    const graphListItem = {
+        "name": listName,
+    };
+
+    return graphClient
+    .api(`/me/outlook/taskFolders`)
+    .post(graphListItem)
+}
+
 /**
-* Adds an outlookTask to the given target list by name. If the list does not 
-* exist it will be created.
+* Adds an outlookTask to the given target list by name. If no shoppint list exists, the
+* default list will be created (Alexa shopping list)
 * 
 * @param graphClient Microsoft graph API client
-* @param {String} listName list name to which the item shall be added
+* @param {String} alexaListName list name to which the item shall be added
 * @param {outlookTask} graphTaskItem task item which shall be added
 * @param {String} consentToken consent token from Alexa request
 */
-const addShoppingItem = (graphClient, listName, graphTaskItem, consentToken) => {
-  //Use existing list if available
-  var filter = `contains(name,'Einkauf')
-  or contains(name,'Shop')
-  or contains(name,'einkauf')
-  or contains(name,'shop')
-  or contains(name,'${listName}')`; 
-  //startswith(name,'${listName}')`;
+const addShoppingItem = async (graphClient, alexaListName, graphTaskItem, consentToken) => {
+    const targetList = await getTargetShoppingLists(graphClient, alexaListName); 
+    console.log(targetList); 
 
-  graphClient
-  .api('/me/outlook/taskFolders')
-  .filter(filter)
-  .count(true)
-  .get()
-  .then((res) => {
-      if (res["@odata.count"] > 0) {
-          //Add to existing list
-          graphClient
-          .api(`/me/outlook/taskFolders/${res.value[0].id}/tasks`)
-          .post(graphTaskItem)
-          .then((res) => {
-              console.log(`${res.subject} was added to list ${listName}`);
-          }).catch((err) => {
-              console.log(err);
-          });
-      } else {
-          //Create new list
-          const graphListItem = {
-              "name": listName,
-          };
-          graphClient
-          .api(`/me/outlook/taskFolders`)
-          .post(graphListItem)
-          .then((res) => {
-              console.log(`${listName} was created`);
-              //Add to created list
-              graphClient
-              .api(`/me/outlook/taskFolders/${res.id}/tasks`)
-              .post(graphTaskItem)
-              .then((res) => {
-                  console.log(`${res.subject} was added to list ${listName}`);
-              }).catch((err) => {
-                  console.log(err);
-              });
-          });
-      }
-      console.log(res);
-  }).catch((err) => {
-      console.log(err);
-  });   
+    if (targetList["@odata.count"] > 0) {
+        //Add to existing list
+        var result = await addTaskToFolder(graphClient, targetList.value[0].id, graphTaskItem);
+        console.log(`${result.subject} was added to list: ${targetList.value[0].name}`);
+    } else {
+        //Create new list
+        var result = await createList(graphClient, alexaListName);
+        console.log(`${result.name} was created`);
+        
+        //Add to created list
+        var result = await addTaskToFolder(graphClient, result.id, graphTaskItem);
+        console.log(`${result.subject} was added to list: ${result.name}`);
+    }  
 };
 
 /**
@@ -66,15 +72,9 @@ const addShoppingItem = (graphClient, listName, graphTaskItem, consentToken) => 
 * @param {outlookTask} taskItem task item which shall be added
 * @param {String} consentToken consent token from Alexa request
 */
-const addToDoItem = (graphClient, graphTaskItem, consentToken) => {
-    graphClient
-        .api(`/me/outlook/tasks`)
-        .post(graphTaskItem)
-        .then((res) => {
-            console.log(`${graphTaskItem.subject} was added to default To-Do list}`);
-        }).catch((err) => {
-            console.log(err);
-    });
+const addToDoItem = async (graphClient, graphTaskItem, consentToken) => {
+    var result = await addTask(graphClient, graphTaskItem);
+    console.log(`${result.subject} was added to default To-Do list}`);
 };
 
 module.exports = {
