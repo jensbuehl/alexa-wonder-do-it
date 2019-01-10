9e9445342516e41bc73ef0d551a47ba2d18f57b1
Jens Bühl <jens.buehl@outlook.com>
2019-01-10 09:43:36 +0000

    Rename "list" with "folder" in request logic to be inline with the graph API type naming.

--- lambda/custom/lib/graphListHelper.js
+++ lambda/custom/lib/graphListHelper.js
@@ -1,11 +1,11 @@
 //Microsoft Graph requests
-async function addTaskToList(graphClient, outlookTask, targetList){
+async function addTaskToFolder(graphClient, outlookTask, outlookTaskFolder){
     try {
         var result = await graphClient
-        .api(`/me/outlook/taskFolders/${targetList.id}/tasks`)
+        .api(`/me/outlook/taskFolders/${outlookTaskFolder.id}/tasks`)
         .post(outlookTask)
 
-        console.log(`${result.subject} was added to list: ${targetList.name}`);
+        console.log(`${result.subject} was added to list: ${outlookTaskFolder.name}`);
         return result;
     } catch (error) {
         console.log(error);
@@ -18,25 +18,25 @@ async function addTaskDefault(graphClient, outlookTask){
         .api(`/me/outlook/tasks`)
         .post(outlookTask)
 
-        console.log(`${result.subject} was added to default To-Do list`);
+        console.log(`${result.subject} was added to default To-Do folder`);
         return result;
     } catch (error) {
         console.log(error);
     }
 }
 
-async function addTask(graphClient, outlookTask, targetList){
-    if (undefined === targetList) {
+async function addTask(graphClient, outlookTask, outlookTaskFolder){
+    if (undefined === outlookTaskFolder) {
         return addTaskDefault(graphClient, outlookTask)
     } else {
-        return addTaskToList(graphClient, outlookTask, targetList);
+        return addTaskToFolder(graphClient, outlookTask, outlookTaskFolder);
     }
 }
 
-async function getTasks(graphClient, filter, targetList){
+async function getTasks(graphClient, filter, outlookTaskFolder){
     try {
         var tasks = await graphClient
-        .api(`/me/outlook/taskFolders/${targetList.id}/tasks`)
+        .api(`/me/outlook/taskFolders/${outlookTaskFolder.id}/tasks`)
         .filter(filter)
         .count(true)
         .get();
@@ -49,17 +49,17 @@ async function getTasks(graphClient, filter, targetList){
     }
 }
 
-async function getLists(graphClient, filter){
+async function getFolders(graphClient, filter){
     try {
-        var lists = await graphClient
+        var folders = await graphClient
         .api('/me/outlook/taskFolders')
         .filter(filter)
         .count(true)
         .get();
 
-        console.log("Received lists:");
-        console.log(lists);
-        return lists;
+        console.log("Received folders:");
+        console.log(folders);
+        return folders;
     } catch (error) {
         console.log(error);
     }
@@ -78,14 +78,14 @@ async function updateTask(graphClient, outlookTask){
     }
 }
 
-async function addList(graphClient, graphListItem){
+async function addFolder(graphClient, outlookTaskFolder){
     try {
-        var list = await graphClient
+        var folder = await graphClient
         .api(`/me/outlook/taskFolders`)
-        .post(graphListItem);
+        .post(outlookTaskFolder);
 
-        console.log(`${list.name} was created.`);
-        return list;
+        console.log(`${folder.name} was created.`);
+        return folder;
     } catch (error) {
         console.log(error);
     }
@@ -93,14 +93,6 @@ async function addList(graphClient, graphListItem){
 
 //Skill logic
 
-/**
-* Adds an outlookTask to the given target list by name. If no shopping list exists, the
-* default list will be created (Alexa shopping list)
-* @param graphClient Microsoft graph API client
-* @param {String} alexaListName list name to which the item shall be added
-* @param {outlookTask} outlookTask task item which shall be added
-* @param {String} listName Originating alexa list name.
-*/
 function buildShoppingListFilter(listName){
     return `contains(name,'einkauf')
             or contains(name,'shop')
@@ -108,37 +100,37 @@ function buildShoppingListFilter(listName){
             or contains(name,'${listName}')`;
 }
 
-function createGraphListItem(listName){
-    return { "name": listName };
+function createOutlookTaskFolder(name){
+    return { "name": name };
 }
 
 async function getShoppingList(graphClient, listName){
     var shoppingListFilter = buildShoppingListFilter(listName);
-    var lists = await getLists(graphClient, shoppingListFilter);
+    var lists = await getFolders(graphClient, shoppingListFilter);
     
     if (lists["@odata.count"] > 0){
         return lists.value[0];
     } else {
-        return createList(graphClient, alexaListName);
+        return createFolder(graphClient, listName);
     }
 }
 
-async function getDefaultList(graphClient){
+async function getDefaultFolder(graphClient){
     try {
-        var allLists = await getLists(graphClient,"");
+        var allLists = await getFolders(graphClient,"");
         return allLists.value.find(x => x.isDefaultFolder === true);
     } catch (error) {
         console.log(error); 
     }
 }
 
-async function createList(graphClient, listName){
-    const graphListItem = createGraphListItem(listName);
-    return await addList(graphClient, listName);
+async function createFolder(graphClient, listName){
+    const graphListItem = createOutlookTaskFolder(listName);
+    return await addFolder(graphClient, listName);
 }
 
-async function handleDuplicates(graphClient, outlookTask, targetList){
-    var duplicates = getDuplicates(graphClient, outlookTask, targetList);
+async function handleDuplicates(graphClient, outlookTask, outlookTaskFolder){
+    var duplicates = getDuplicates(graphClient, outlookTask, outlookTaskFolder);
     if (duplicates["@odata.count"] > 0){
         //Set completed duplicate task back to notStarted
          var duplicate = duplicates.value.find(x => x.status === "completed");
@@ -150,12 +142,12 @@ async function handleDuplicates(graphClient, outlookTask, targetList){
     return false;
 }
 
-async function getDuplicates(graphClient, outlookTask, targetList){
+async function getDuplicates(graphClient, outlookTask, outlookTaskFolder){
     var duplicateFilter = `subject eq '${outlookTask.subject}'`
-    if (undefined === targetList) {
-        var targetList = await getDefaultList(graphClient);
+    if (undefined === outlookTaskFolder) {
+        var outlookTaskFolder = await getDefaultFolder(graphClient);
     }
-    return await getTasks(graphClient, duplicateFilter, targetList);
+    return await getTasks(graphClient, duplicateFilter, outlookTaskFolder);
 }
 
 /**
@@ -167,10 +159,10 @@ async function getDuplicates(graphClient, outlookTask, targetList){
 * @param {String} consentToken consent token from Alexa request
 */
 const addShoppingItem = async (graphClient, alexaListName, outlookTask, consentToken) => {
-    const targetList = await getShoppingList(graphClient, alexaListName); 
-    if (false === await handleDuplicates(graphClient, outlookTask, targetList)){
+    const outlookTaskFolder = await getShoppingList(graphClient, alexaListName); 
+    if (false === await handleDuplicates(graphClient, outlookTask, outlookTaskFolder)){
         //No duplicates found!
-        addTask(graphClient, outlookTask, targetList);
+        addTask(graphClient, outlookTask, outlookTaskFolder);
     }
 };
 
