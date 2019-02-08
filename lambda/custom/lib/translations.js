c4983003ab9d10cc5cdf88583d9c7b865cd0e6bf
Jens Bühl <jens.buehl@outlook.com>
2019-02-08 08:21:43 +0000

    Updated translations.js with Italian and Spanish

--- lambda/custom/lib/translations.js
+++ lambda/custom/lib/translations.js
@@ -33,12 +33,40 @@ const translations = {
             SKILL_NAME: 'To-Do Magique',
             WELCOME_MESSAGE: 'Bienvenue à To-Do magique ! Utilisez Alexa pour ajouter de nouveaux items à vos listes ! Vous pouvez dire : "Alexa, ajoute pain à ma liste d\'achat" ou "Alexa, ajoute magasinage à ma liste de choses à faire".',
             HELP_MESSAGE: 'Vous pouvez dire "Alexa, ajoute pain à ma liste d\'achats" ou "Alexa, ajoute magasinage à ma liste de choses à faire".',
-            HELP_REPROMPT: 'Utilisez Alexa pour ajouter de nouveaux items à vos listes !',
+            HELP_REPROMPT: 'Utilisez Alexa pour ajouter de nouveaux items à vos listes!',
             STOP_MESSAGE: 'Aurevoir!',
             PERMISSIONS_MISSING_CARD_TITLE: 'To-Do Magique - Permissions insuffisantes',
             PERMISSIONS_MISSING_MESSAGE: 'Pour démarrer l\'utilisation de cette skill, svp utiliser l\'application mobile Amazon Alexa pour accepter les permissions requises.',
             ERROR_MESSAGE: 'Je suis désolé. Je ne peux prendre en charge votre requête en raison de difficultés techniques.',
             LINK_ACCOUNT_MESSAGE: 'Pour démarrer l\'utilisation de cette skill, svp utiliser l\'application mobile Amazon Alexa pour vous authentifier avec Amazon. Plus d\'information a été envoyé à votre accueil Alexa.',
+            SHOPPING_LIST: 'liste d\'achats'
+        },
+    },
+    'it': {
+        translation: {
+            SKILL_NAME: 'To-Do Miracolo',
+            WELCOME_MESSAGE: 'Benvenuto su To-Do Miracolo! Utilizza Alexa per aggiungere nuovi elementi alle tue liste! Puoi dire, "Alexa, aggiungi il pane alla mia lista della spesa" oppure "Alexa, aggiungi \'Fare shopping" alla mia lista To-do.',
+            HELP_MESSAGE: 'Puoi dire, "Alexa, aggiungi il pane alla mia lista della spesa" oppure "Alexa, aggiungi \'Fare shopping" alla mia lista To-do.',
+            HELP_REPROMPT: 'Usa Alexa per aggiungere nuovi elementi alle tue liste!',
+            STOP_MESSAGE: 'Arrivederci!',
+            PERMISSIONS_MISSING_CARD_TITLE: 'To-Do Miracolo - Permessi mancanti',
+            PERMISSIONS_MISSING_MESSAGE: 'Per un corretto funzionamento della skill, perfavore accetta le  richieste di permessi dello step successivo.',
+            ERROR_MESSAGE: 'Mi spiace, a causa di una difficoltà tecnica non posso aiutarti.',
+            LINK_ACCOUNT_MESSAGE: 'Per utilizzare questa skill, perfavore segui lo step successivo per autenticarti con l\'account Amazon. Maggiori informazioni sono state spedite al vostro account Alexa.',
+            SHOPPING_LIST: 'liste d\'achats'
+        },
+    },
+    'es': {
+        translation: {
+            SKILL_NAME: 'To-Do Maravillosa',
+            WELCOME_MESSAGE: '¡Bienvenido a To-Do Maravillosa! Utiliza a Alexa para agregar nuevos elementos a tus listas. Puedes decir: "Alexa, añade pan a mi lista de compras" o "Alexa, añade compras a mi lista de quéhaceres".',
+            HELP_MESSAGE: 'Puedes decir: "Alexa, añade pan a mi lista de compras" o "Alexa, añade compras a mi lista de quéhaceres".',
+            HELP_REPROMPT: 'Utiliza a Alexa para agregar nuevos elementos a tus listas.',
+            STOP_MESSAGE: '¡Hasta luego!',
+            PERMISSIONS_MISSING_CARD_TITLE: 'To-Do Maravillosa - Permisos insuficientes.',
+            PERMISSIONS_MISSING_MESSAGE: 'Para iniciar esta habilidad, favor de utilizar la aplicación móvil para confirmar los permisos de acceso a la lista requerida.',
+            ERROR_MESSAGE: 'Lo siento, no puedo realizar esta tarea debido a problemas técnicos.',
+            LINK_ACCOUNT_MESSAGE: 'Para utilizar esta habilidad, favor de utilizar la aplicación móvil de Alexa para autentificarte con tu cuenta de Amazon. Más detalles han sido enviados a tu página de inicio de Alexa.',
             SHOPPING_LIST: 'liste d\'achats'
         },
     },
